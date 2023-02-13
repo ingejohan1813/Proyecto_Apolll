fa2c478ca67f4610f428cc75055923dce7a53ed0
CristhianDPadilla <124319558+CristhianDPadilla@users.noreply.github.com>
2023-02-12 19:20:03 -0500

    Ejemplo

--- Perro.java
+++ Perro.java
@@ -1,16 +1,31 @@
 class Perro {
 
+
+   
     //atributos
 
+    private String nombre;
+    private String raza; 
+    private String color;
+    private String genero;
+    private int edad;
 
-
-
-
-
+    public perro (){
+        nombre = "";
+        raza = "";
+        color = "";
+        genero = "";
+        edad = 0;
+    }
 
 
     //métodos 
-   
+   public String alimentar; {
+
+    Return system.out.println("Toy comiendo ");
+
+   }
+
 
 
 
