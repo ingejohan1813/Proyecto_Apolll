3e8a71438281911f85347a17d38d6785acb99fc2
ingejohan1813 <johans.ordonez221@umariana.edu.co>
2023-02-12 19:47:43 -0500

    Agregando_atributos

--- Gato.java
+++ Gato.java
@@ -1,4 +1,15 @@
  class Gato{
+    String nombre;
+    int edad;
+    double peso;
+    String raza;
+
+    public Gato (String nombre, int edad, double peso, String raza) {
+       this.nombre = nombre;
+       this.edad = edad;
+       this.peso = peso;
+       this.raza = raza;
 
     
  }
+}
