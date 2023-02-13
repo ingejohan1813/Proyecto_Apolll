dc12bd7036d6033e268ed0670fd351c2c5417b8f
sofiburbano12 <124370721+sofiburbano12@users.noreply.github.com>
2023-02-12 21:12:34 -0500

    Update Gato.java

--- Gato.java
+++ Gato.java
@@ -1,6 +1,23 @@
  class Gato{
 // atributos 
-
+private String maullar;
+private String dormir;
+private String ronronear;
+private String lamer;
+private String edad;
 // metodos 
+public void maullar () {
+ System.out.println("miau miau");
+}
+ public void dormir () {
+ System.out.println("Zzzz Zzzz");
+}
+  public void ronronear () {
+ System.out.println("prrrr prrr");
+}
+  public void lamer () {
+ System.out.println("meowww meoww");
+}
+  
     
  }
